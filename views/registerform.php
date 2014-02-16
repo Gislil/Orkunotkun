428a1e8f228e58ea5e5dd0c8ebd778f6d6ef8d4a
Helgi Hardarson <helgihardarson@gmail.com>
2014-02-16 18:22:10 +0000

    small stuff

--- views/registerform.php
+++ views/registerform.php
@@ -1,27 +1,3 @@
-<?php
-    // if (isset($register)) {
-    //     if (empty($register->errorMessages)) {
-    //         echo $register->message;
-    //     } else {
-    //         foreach ($register->errorMessages as $error) {
-    //             echo $error;
-    //         }
-    //     }
-    // }
-?>
-<!-- 
-<form action="register.php" method="post" name="registerform">
-
-    <label for="email">Tölvupóstfang</label>
-    <input type="email" name="email" id="email">
-
-    <label for="password">Lykilorð (minnst 8 stafir)</label>
-    <input type="password" name="password">
-
-    <input type="submit" name="register">
-    
-</form> -->
-
 <div class="error_messages">
     <?php
         if (isset($login) AND !empty($login->errorMessages)) {
